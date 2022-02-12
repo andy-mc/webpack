a8b03c4e423bd960ab8844f538c7f7f420547ba0
Andrés Muñoz <andres.munoz@idukay.com>
2022-02-12 15:59:31 -0500

    addd sum

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-// entry point of webpack, main point
+import {sum} from './utils/sum';
 
 const hello_world = 'hello world !!';
 console.log('hello_world:', hello_world);
+const total = sum(12, 12);
+console.log('total:', total)
